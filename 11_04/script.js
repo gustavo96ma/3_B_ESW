0d34f74112a971dcd3a823571a8d94747f1313a8
gustavo96ma <gustavo96ma@gmail.com>
2025-04-11 19:10:07 -0300

    11/04

---
+++ 11_04/script.js
@@ -0,0 +1,26 @@
+console.log("Hello World!");
+
+var frase = "Noooossa, é muito cremoso!";
+console.log(frase);
+
+var numerico = 51;
+console.log(numerico);
+
+var booleano = false;
+console.log(booleano);
+
+var decimal = 0.15;
+console.log(decimal);
+
+var lista = [true, 51, "café"];
+console.log(lista);
+
+var dicionario = { 1: "valor1", chave2: "valor2" };
+console.log(dicionario);
+
+for (var i = 99; i < 100; i--) {
+  console.log(i);
+  if (i === 1) {
+    break;
+  }
+}
